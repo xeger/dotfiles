c13c963522f36b70b18743784ab680543e948330
Tony Spataro <tony@rightscale.com>
2016-07-01 22:32:43 -0700

    Fix final bugs in bash bootstrap

--- bootstrap-bash.sh
+++ bootstrap-bash.sh
@@ -13,13 +13,14 @@
 __dotfiles_secure__() {
   # Check for user-specific profile, or use default (_).
   if [ -d profiles/$USER ]; then
-    subdir=profiles/$USER
-    pubkey=keys/$USER
+    user=$USER
   else
-    subdir=profiles/_
-    pubkey=keys/_.asc
+    user=_
     needRootTrust=1
   fi
+
+  subdir="profiles/$user"
+  pubkey="keys/${user}.asc"
 
   # Determine ID of the key stored in the pubkey file.
   owner=`gpg $pubkey | grep '^pub' | cut -d ' ' -f 3 | cut -d / -f 2`
@@ -34,9 +35,9 @@ __dotfiles_secure__() {
   fi
 
   if [ -n "$needRootTrust" ]; then
-    gpg --list-keys --list-options show-uid-validity $signer | grep -q "\[ultimate\]"
+    gpg --list-keys --list-options show-uid-validity $signer | grep -q '\[ultimate\]'
   else
-    gpg --list-keys --list-options show-uid-validity $signer | grep -q "\[\(full|ultimate\)\] ${user}@localhost"
+    gpg --list-keys --list-options show-uid-validity $signer | grep -q "\\[\\(full|ultimate\\)\\]\\ ${user}@localhost"
   fi
   if [ $? == 0 ]; then
     echo "Applying $user profile signed by ($signer)"
@@ -52,7 +53,7 @@ __dotfiles_secure__() {
   fi
 
   if [ -d $subdir ]; then
-    echo "Integrity-check profile"
+    echo "Integrity-checking profile"
     cd $subdir
 
     # Find every commit that has ever touched any file in, or under, this dir.
@@ -69,24 +70,30 @@ __dotfiles_secure__() {
         return 21
       fi
 
-      gpg --list-keys --list-options show-uid-validity $signer | grep -Eq "\[\(full|ultimate\)\] ${user}@localhost"
+      pat='\[\(full|ultimate\)\]'
+      pat="${pat} ${user}@localhost"
+      gpg --list-keys --list-options show-uid-validity $signer | grep -Eq pat
       if [ $? != 0 ]; then
-        echo "ERROR: no proof of identity for commit signer (${}) of ${commit}"
+        echo "ERROR: no proof of identity for commit signer (${signer}) of ${commit}"
         return 22
       fi
     done
+
+    shopt -s dotglob
+    echo "Installing profile to $HOME"
+    cp -Rf * $HOME
   else
     echo "ERROR: missing custom profile; $subdir not found under $PWD"
-    return 100
+    return 30
   fi
 }
 
 pushd /var/lib/dotfiles
-
 __dotfiles_secure__
+popd
 
-if [ $? != 0 ]; then
+if [ $? == 0 ]; then
+  source .bash_profile
+else
   echo "Customized profile is disabled for your security; using system default"
 fi
-
-popd
